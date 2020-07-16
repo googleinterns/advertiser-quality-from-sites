a6a676a0301ef72d98fc9a5c67c98c26e7a8b60f
beginner1010 <vahid.sanei@gmail.com>
2020-07-16 02:35:21 +0000

    --

--- source/get_url_links.py
+++ source/get_url_links.py
@@ -1,3 +1,6 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 HOME_PATH = '/home/vahidsanei_google_com/'
 PATH_DRIVER = '/home/vahidsanei_google_com/chromedriver/chromedriver'
 
@@ -23,45 +26,51 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+
 def get_urls(path):
-    file = open(path, 'r')
-    urls = [line.strip() for line in file.readlines()]
-    return urls
+	file = open(path, 'r')
+	urls = [line.strip() for line in file.readlines()]
+	return urls
+
 
 #   with open(path, 'r') as file:
 #     s = file.read()
 #   s = re.sub('[\[\]\']', ' ', s)
 #   urls = s.split(',')
-  # some found urls (not many! a few of them) are digits! Recheck yelp.com html file to resolve the issues.
-  #urls = [None if (url.strip() == 'None' or url.strip()[0].isdigit()) else url.strip() for url in urls]
+# some found urls (not many! a few of them) are digits! Recheck yelp.com html file to resolve the issues.
+# urls = [None if (url.strip() == 'None' or url.strip()[0].isdigit()) else url.strip() for url in urls]
 #  return urls
 
 def get_yelp_data(path):
-  with open(path, 'r') as file:
-    raw_data = file.readlines()
-  raw_data = map(lambda x: x.rstrip(), raw_data)
-  json_data = '[' + ','.join(raw_data) + ']'
-  df = pd.read_json(json_data)
-  return df
+	with open(path, 'r') as file:
+		raw_data = file.readlines()
+		raw_data = map(lambda x: x.rstrip(), raw_data)
+		json_data = '[' + ','.join(raw_data) + ']'
+	df = pd.read_json(json_data)
+	return df
 
-def save_content_of_websites(output_path=None):
-  path = os.path.join(HOME_PATH, 'data/yelp_data', 'yelp_academic_dataset_business.json')
-  df = get_yelp_data(path)
-  n = len(df)
 
-  urls = get_urls(os.path.join(HOME_PATH,'data/yelp_data','link_list.txt'))
-  
-  with open(os.path.join(HOME_PATH,'data/yelp_data','test.txt'), 'w') as f:
-        for url in urls:
-            if url is None: f.write('None')
-            else: f.write(url + '')
-            f.write('\n')
-    
-  print('size of url list = {}'.format(len(urls)))
-  urls = urls + list(np.nan for _ in range(n - len(urls)))
+def save_content_of_websites():
+	path = os.path.join(HOME_PATH, 'data/yelp_data', 'yelp_academic_dataset_business.json')
+	df = get_yelp_data(path)
+	n = len(df)
 
-  df['url'] = urls
-  df = df[pd.notnull(df['url'])]
+	urls = get_urls(os.path.join(HOME_PATH, 'data/yelp_data', 'link_list.txt'))
+
+	#with open(os.path.join(HOME_PATH, 'data/yelp_data', 'test.txt'), 'w') as f:
+		#for url in urls:
+			#if url is None:
+				#f.write('None')
+			#else:
+				#f.write(url + '')
+				#f.write('\n')
+
+	print('size of url list = {}'.format(len(urls)))
+	urls = [np.nan if url == 'None' or url.isdigit() else url for url in urls]
+	urls = urls + list(np.nan for _ in range(n - len(urls)))
+
+	df['url'] = urls
+	df = df[pd.notnull(df['url'])]
 
 #   options = Options()
 #   options.add_argument('--headless')
@@ -77,42 +86,62 @@ def save_content_of_websites(output_path=None):
 #         except:
 #             webpage_content.append(np.nan)
 #         #print(webpage_content[-1])
-                                                       
 
-  webpage_content = []
-  c, not_found = 0, 0
-  for url in tqdm(df['url']):
-    webpage_content.append(None)
-    try:
-      page = requests.get(url, stream=True, timeout=8.0)
-      page.encoding = 'utf-8'
-      webpage_content[-1] = page.content
-    except:
-      not_found += 1
-    c += 1    
-  print(len(webpage_content), c)
+	webpage_content = []
+	c, not_found = 0, 0
+	#session = HTMLSession()
+	
+	for url in tqdm(df['url']):
+		webpage_content.append(None)
+		if url == 'None':
+			continue
+		try:
 
-  n = len(df)
-  webpage_content = webpage_content + list(np.nan for _ in range(n - len(webpage_content)))
-  df['webpage_text'] = webpage_content
-  df = df[pd.notnull(df['webpage_text'])]
-                                           
-  #dt = str(datetime.datetime.now())
-  #dt = dt[:dt.find('.')].replace(' ', '_').replace(':','-')i
-  #file_name = 'business_{}.csv'.format(dt)
-  if output_path is None:
-    file_name = 'business.csv'
+			page = requests.get(url, stream=True, timeout=5.0)
+			page.encoding = 'utf-8'
+			webpage_content[-1] = page.content
+			
+			#r = session.get(url, stream=True, timeout=6.0)
+			#r.html.render()
+			#webpage_content[-1] = r.html.text
+			#print(webpage_content[-1])
+			
+		except Exception as e:
 
-    folder_path = os.path.join(HOME_PATH, 'data/yelp_data', 'updated')
-    if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
-    file_path = os.path.join(folder_path, file_name)
-  else:
-    file_path = os.path.join(output_path)
+			# print(webpage_content[-1])
+			# not_found += 1
+			# frac = not_found / (c + 1.0)
+			# print(frac)
 
-  df.to_csv(file_path, index=False, header=True)
-  print('Check out{}'.format(str(file_path)))
-  #pd.set_option('display.max_columns', None)
+			#print(e)
+			c += 1
+			
+	print (len(webpage_content), c)
+	
+	n = len(df)
+	webpage_content = webpage_content + list(np.nan for _ in range(n - len(webpage_content)))
+	df['webpage_text'] = webpage_content
+	df = df[pd.notnull(df['webpage_text'])]
+
+	# dt = str(datetime.datetime.now())
+	# dt = dt[:dt.find('.')].replace(' ', '_').replace(':','-')i
+	# file_name = 'business_{}.csv'.format(dt)
+
+	file_name = 'business.csv'
+
+	folder_path = os.path.join(HOME_PATH, 'data', 'yelp_data', 'updated_test')
+	if not os.path.exists(folder_path):
+		os.makedirs(folder_path)
+	
+	file_path = os.path.join(folder_path, 'business.csv')
+
+	df.to_csv(file_path, index=False, header=True)
+	print('Check out{}'.format(file_path))
+
+
+	# pd.set_option('display.max_columns', None)
 
 if __name__ == '__main__':
-  save_content_of_websites()
+	save_content_of_websites()
+
+			
