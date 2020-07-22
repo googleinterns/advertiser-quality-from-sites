b89b33cf00d6da44b4cadc38e838aa5224791105
beginner1010 <vahid.sanei@gmail.com>
2020-07-22 15:49:39 +0000

    recursive model is added

---
+++ source/recursive_model/recursive_rnn.py
@@ -0,0 +1,375 @@
+import sys, os, math, time, itertools, shutil
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow.compat.v1 as tf
+import tensorflow as tftwo
+from tensorflow import keras
+from bert import BertModelLayer
+from bert.loader import StockBertConfig, map_stock_config_to_params, load_stock_weights
+from bert.tokenization.bert_tokenization import FullTokenizer
+
+import tree_lib
+
+class Configuration():
+	def __init__(self, input_df=None, lr=1e-2, n_epochs=20, l2=0.02, test_split_ratio=0.2, val_split_ratio=None, anneal_factor=1.5, 
+			anneal_tolerance=0.98, patience_early_stopping=5, max_depth=None, n_classes=None, bert_folder_path=None, 
+			bert_embedding_size=768, embedding_size=300, max_content_length=50):
+		assert input_df is not None, 'input df cannot be None'
+		assert n_classes is not None, '# of classes must be specified'
+		assert bert_folder_path is not None, 'input files for BERT(uncased_L-12_H-768_A-12) must be specified'
+
+		self.input_df = input_df
+		self.lr = lr
+		self.n_epochs = n_epochs
+		self.l2 = l2
+		self.test_split_ratio = test_split_ratio
+		self.val_split_ratio = val_split_ratio
+		self.anneal_factor = anneal_factor
+		self.anneal_tolerance = anneal_tolerance
+		self.patience_early_stopping = patience_early_stopping
+		self.max_depth = max_depth
+		self.n_classes = n_classes
+		
+		self.bert_folder_path = bert_folder_path
+		self.bert_ckpt_file = os.path.join(self.bert_folder_path, 'bert_model.ckpt')
+		self.bert_config_file = os.path.join(self.bert_folder_path, 'bert_config.json')
+		self.tokenizer = FullTokenizer(vocab_file=os.path.join(self.bert_folder_path, 'vocab.txt'))
+		# the output size of uncased_L-12_H-768_A-12 is 768
+		self.bert_embedding_size = bert_embedding_size
+		self.embedding_size = embedding_size
+		sys.stdout.write('WARNING: This implementation uses BERT model uncased_L-12_H-768_A-12\n')
+		sys.stdout.flush()
+		self.max_content_length = max_content_length
+
+		self.model_name = f'recnn_l2={self.l2}_lr={self.lr}_depth={self.max_depth}_cls={self.n_classes}'
+		self.weights_path = os.path.join('weights', self.model_name)
+		# save weights after set value steps, to remove the constructed trees from RAM (to avoid OOM)
+		self.AVOID_OOM_AT = 100
+	
+class RecursiveModel():
+	
+	def __init__(self, configuration):
+		self.configuration = configuration
+		
+		return
+		
+		trees = tree_lib.read_trees(self.configuration.input_df)		
+		self.bert_model = self.load_bert()
+		
+		sys.stdout.write('[Preprocessing step] Get embeddings of node texts:\n')
+		sys.stdout.flush()
+		for tree in tqdm(trees):
+			self.__content_to_embedding__(tree.root)
+			
+		tf.disable_v2_behavior()
+		tf.disable_eager_execution()
+		
+		split_size = int((1.0 - self.configuration.test_split_ratio) * len(trees))
+		self.train_trees = trees[:split_size]
+		self.test_trees = trees[split_size:]
+		self.val_trees = None
+		if self.configuration.val_split_ratio is not None:
+			split_size = int((1.0 - self.configuration.val_split_ratio) * len(self.train_trees))
+			self.val_trees = self.train_trees[split_size:]
+			self.train_trees = self.train_trees[:split_size]
+			
+		
+	def __content_to_embedding__(self, node):
+		if node.is_leaf == True:
+			node.bert_embedding = np.asarray(self.bert_model.predict([self.__cut_with_padding__(node.content)]))
+		else:
+			for child in node.children:
+				self.__content_to_embedding__(child)
+	
+	def __cut_with_padding__(self, content):
+		tokens = self.configuration.tokenizer.tokenize(content)
+		ids = self.configuration.tokenizer.convert_tokens_to_ids(tokens)
+		# pad the remaining cells with zero
+		ids = ids + [0 for _ in range(self.configuration.max_content_length - len(ids))]
+		ids = ids[:self.configuration.max_content_length]
+		return ids
+	
+	def load_bert(self):
+		try:
+			with tftwo.io.gfile.GFile(self.configuration.bert_config_file, 'r') as gf:
+				bert_config = StockBertConfig.from_json_string(gf.read())
+				bert_params = map_stock_config_to_params(bert_config)
+				bert_params.adapter_size = None
+				bert = BertModelLayer.from_params(bert_params, name='bert')
+		except Exception as e:
+			print(e)
+			raise e
+			
+		input_ = keras.layers.Input(shape=(self.configuration.max_content_length, ), dtype='int64', name="input_ids")
+		x = bert(input_)
+		output_ = keras.layers.Lambda(lambda seq: seq[:,0,:])(x)
+		model = keras.Model(inputs=input_, outputs=output_)
+		model.build(input_shape=(None, self.configuration.max_content_length))
+		load_stock_weights(bert, self.configuration.bert_ckpt_file)
+		return model
+	
+	def get_tensor(self, node):			
+		if node.is_leaf == True:
+			with tf.variable_scope('bert_layer', reuse=True):
+				W_bert = tf.get_variable('W_bert')
+				b_bert = tf.get_variable('b_bert')
+			node_tensor = tf.nn.relu(tf.matmul(node.bert_embedding, W_bert) + b_bert)
+		else:	
+			children_tensors_list = []
+			for child in node.children:
+				children_tensors_list.append(self.get_tensor(child))
+			children_tensors = tf.concat(children_tensors_list, 1)
+							
+			with tf.variable_scope(f'layer{len(node.children)}', reuse=True):
+				W = tf.get_variable(f'W{len(node.children)}')
+				b = tf.get_variable(f'b{len(node.children)}')
+			node_tensor = tf.nn.relu(tf.matmul(children_tensors, W) + b)
+			
+		return node_tensor
+	
+	def _compute_logit(self, tree):
+		root_tensor = self.get_tensor(tree.root)
+		with tf.variable_scope('output_layer', reuse=True):
+			W = tf.get_variable('W0')
+			b = tf.get_variable('b0')
+		logit = tf.matmul(root_tensor, W) + b
+		return logit
+		
+	def get_variables(self):
+		with tf.variable_scope('output_layer'):
+			tf.get_variable('W0', [self.configuration.embedding_size, self.configuration.n_classes])
+			tf.get_variable('b0', [1, self.configuration.n_classes])
+			
+		for d in range(1, self.configuration.max_depth + 1):
+			with tf.variable_scope(f'layer{d}'):
+				tf.get_variable(f'W{d}', [self.configuration.embedding_size * d, self.configuration.embedding_size])
+				tf.get_variable(f'b{d}', [1, self.configuration.embedding_size])
+
+		with tf.variable_scope('bert_layer'):
+			tf.get_variable('W_bert', [self.configuration.bert_embedding_size, self.configuration.embedding_size])
+			tf.get_variable('b_bert', [1, self.configuration.embedding_size])
+	
+	def _compute_loss(self, label, logit):
+		cross_entropy_loss = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=tf.constant(label)))
+		
+		Ws = []
+		with tf.variable_scope('output_layer', reuse=True):
+			W = tf.get_variable('W0')
+			Ws.append(W)
+		for d in range(1, self.configuration.max_depth + 1):
+			with tf.variable_scope(f'layer{d}', reuse=True):
+				W = tf.get_variable(f'W{d}')
+				Ws.append(W)
+		with tf.variable_scope('bert_layer', reuse=True):
+			W = tf.get_variable('W_bert')
+			Ws.append(W)
+			
+		total_weight_loss = 0
+		for W in Ws:
+			total_weight_loss += tf.nn.l2_loss(W)
+		
+		total_loss = self.configuration.l2 * total_weight_loss + cross_entropy_loss
+		return total_loss
+	
+	def tree_loss(self, tree, logit=None):
+		if logit is None: logit = self._compute_logit(tree)
+		return self._compute_loss([tree.label], logit)
+		
+	def run_epoch(self, new_model, epoch_number):
+		losses = []
+		pos = 0
+		weights_path = f'weights/{self.configuration.model_name}.temp'
+		while pos < len(self.train_trees):
+			with tf.Graph().as_default(), tf.Session() as sess:
+				self.get_variables()
+				
+				if new_model == True:
+					sess.run(tf.global_variables_initializer())
+				else:
+					saver = tf.train.Saver()
+					saver.restore(sess, weights_path)
+				
+				for _ in range(self.configuration.AVOID_OOM_AT):
+					if pos >= len(self.train_trees): break
+		
+					tree = self.train_trees[pos]
+					loss = self.tree_loss(tree)
+					optimizer = tf.train.GradientDescentOptimizer(self.configuration.lr).minimize(loss)
+					l, _ = sess.run([loss, optimizer])
+					losses.append(l)
+					
+					sys.stdout.write(f'\r Epoch={epoch_number + 1}, {pos + 1}/{len(self.train_trees)}: loss = {np.mean(losses)}')
+					sys.stdout.flush()
+					pos += 1
+					
+				saver = tf.train.Saver()
+				if not os.path.exists('./weights'):
+					os.makedirs('./weights')
+				saver.save(sess, weights_path)
+		
+		train_predictions, train_losses = self.predict(self.train_trees, weights_path=weights_path)
+		val_predictions, val_losses = self.predict(self.val_trees, weights_path=weights_path)
+		
+		train_labels = [t.label for t in self.train_trees]
+		val_labels = [t.label for t in self.val_trees]
+		
+		train_accuracies = np.equal(train_labels, train_predictions)
+		val_accuracies = np.equal(val_labels, val_predictions)
+		
+		return train_accuracies, val_accuracies, train_losses, val_losses
+						
+	def predict(self, trees, weights_path):
+		predictions, losses = [], []
+		pos = 0
+		sys.stdout.write('Prediction:\n')
+		rogbar = keras.utils.Progbar(len(trees))
+		while pos < len(trees):
+			with tf.Graph().as_default(), tf.Session() as sess:
+				self.get_variables()
+				saver = tf.train.Saver()
+				saver.restore(sess, weights_path)
+				for _ in range(self.configuration.AVOID_OOM_AT):
+					if pos >= len(trees): break
+					
+					tree = trees[pos]
+					logit = self._compute_logit(tree)
+					tf_prediction = tf.argmax(logit, 1)
+					y_pred = sess.run(tf_prediction)
+					predictions.append(y_pred)
+					tree_loss = sess.run(self.tree_loss(tree, logit=logit))
+					losses.append(tree_loss)
+					
+					pos += 1
+					rogbar.update(pos)
+		return predictions, losses
+			
+	def train_model(self):
+		train_losses = []
+		train_accuracies = []
+		val_losses = []
+		val_accuracies = []
+		last_epoch_loss = float('inf')
+		best_val_loss = float('inf')
+		best_val_epoch = -1
+		
+		for epoch in range(self.configuration.n_epochs):		
+			new_model = True if epoch == 0 else False
+			epoch_train_accuracies, epoch_val_accuracies, epoch_train_losses, epoch_val_losses = self.run_epoch(new_model=new_model, epoch_number=epoch)
+			train_acc, val_acc = np.mean(epoch_train_accuracies), np.mean(epoch_val_accuracies)
+			sys.stdout.write(f' Training acc={train_acc}, Validation acc = {val_acc}\n')
+			sys.stdout.flush()
+			
+			train_losses.append(np.mean(epoch_train_losses))
+			train_accuracies.append(np.mean(epoch_train_accuracies))
+			val_losses.append(np.mean(epoch_val_losses))
+			val_accuracies.append(np.mean(epoch_val_accuracies))
+			
+			if train_losses[-1] > last_epoch_loss * self.configuration.anneal_tolerance:
+				self.configuration.lr /= self.configuration.anneal_factor
+				#sys.out.write(f'\n *** Annealed learning rate to {self.configuration.lr} *** \n')
+			last_epoch_loss = train_losses[-1]
+			
+			if val_losses[-1] < best_val_loss:
+				# TODO: for testing phase, save the current model in best_weights.temp 
+				best_val_loss = val_losses[-1]
+				best_val_epoch = epoch
+			
+			#if epoch - best_val_epoch > self.configuration.patience_early_stopping:
+			#	break
+
+class RecursiveModelWithLessParams(RecursiveModel):
+	def __init__(self, configuration):
+		super().__init__(configuration)
+		self.configuration.model_name = f'recnnwlw_l2={self.l2}_lr={self.lr}_depth={self.max_depth}_cls={self.n_classes}' # wlw = with less weights
+	
+	# overrided method
+	def get_tensor(self, node):			
+		if node.is_leaf == True:
+			with tf.variable_scope('bert_layer', reuse=True):
+				W_bert = tf.get_variable('W_bert')
+				b_bert = tf.get_variable('b_bert')
+			node_tensor = tf.nn.relu(tf.matmul(node.bert_embedding, W_bert) + b_bert)
+		else:	
+			with tf.variable_scope(f'layer_internal', reuse=True):
+				W = tf.get_variable(f'W_internal')
+				b = tf.get_variable(f'b_internal')
+
+			for idx, child in enumerate(node.children)):
+				if idx == 0: node_tensor = self.get_tensor(child)
+				else:
+					children_tensors = tf.concat([node_tensor, self.get_tensor(child)], 1)
+					node_tensor = tf.nn.relu(tf.matmul(children_tensors, W) + b)
+								
+		return node_tensor
+	
+	# overrided method
+	def _compute_loss(self, label, logit):
+		cross_entropy_loss = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=tf.constant(label)))
+		
+		Ws = []
+		with tf.variable_scope('output_layer', reuse=True):
+			W = tf.get_variable('W0')
+			Ws.append(W)
+
+		with tf.variable_scope(f'layer_internal', reuse=True):
+			W = tf.get_variable(f'W_internal')
+			Ws.append(W)
+			
+		with tf.variable_scope('bert_layer', reuse=True):
+			W = tf.get_variable('W_bert')
+			Ws.append(W)
+			
+		total_weight_loss = 0
+		for W in Ws:
+			total_weight_loss += tf.nn.l2_loss(W)
+		
+		total_loss = self.configuration.l2 * total_weight_loss + cross_entropy_loss
+		return total_loss
+	
+	# overrided method
+	def get_variables(self):
+		with tf.variable_scope('output_layer'):
+			tf.get_variable('W0', [self.configuration.embedding_size, self.configuration.n_classes])
+			tf.get_variable('b0', [1, self.configuration.n_classes])
+			
+		with tf.variable_scope(f'layer_internal'):
+			tf.get_variable(f'W_internal', [self.configuration.embedding_size * 2, self.configuration.embedding_size])
+			tf.get_variable(f'b_internal', [1, self.configuration.embedding_size])
+
+		with tf.variable_scope('bert_layer'):
+			tf.get_variable('W_bert', [self.configuration.bert_embedding_size, self.configuration.embedding_size])
+			tf.get_variable('b_bert', [1, self.configuration.embedding_size])
+
+
+def test_model():
+	bert_folder_path = os.path.join('/home/vahidsanei_google_com/', 'data','uncased_L-12_H-768_A-12')
+	df = pd.read_csv('/home/vahidsanei_google_com/data/yelp_data/trees/df_with_trees.csv')
+	#df = df[:50]
+	configuration = Configuration(input_df=df, val_split_ratio=0.1, max_depth=4, n_classes=10, bert_folder_path=bert_folder_path)
+	
+	model = RecursiveModel(configuration)
+	
+	start_time = time.time()
+	model.train_model()
+	print(f'Training time = {time.time() - start_time}')
+	
+def test_model2():
+	bert_folder_path = os.path.join('/home/vahidsanei_google_com/', 'data','uncased_L-12_H-768_A-12')
+	df = pd.read_csv('/home/vahidsanei_google_com/data/yelp_data/trees/df_with_trees.csv')
+	configuration = Configuration(input_df=df, val_split_ratio=0.1, n_classes=10, bert_folder_path=bert_folder_path) 
+	model = RecursiveModelWithLessParams(configuration)	
+	
+	start_time = time.time()
+	model.train_model()
+	print(f'Training time = {time.time() - start_time}')
+			
+if __name__ == '__main__':
+	test_model2()
+	
+#shutil.copyfile('./weights/%s.temp.data-00000-of-00001'%self.configuration.model_name, './weights/%s.data-00000-of-00001'%self.configuration.model_name)
+#shutil.copyfile('./weights/%s.temp.index'%self.configuration.model_name, './weights/%s.index'%self.configuration.model_name)
+#shutil.copyfile('./weights/%s.temp.meta'%self.configuration.model_name, './weights/%s.meta'%self.configuration.model_name)
---
+++ source/recursive_model/tree_lib.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import sys, os, re
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+from bs4.element import Comment, Tag, NavigableString
+
+sys.path.append(os.getcwd() + '/..')
+from my_models import unify_yelp_data_classes
+
+class Tree:
+	def __init__(self, encoded_tree):
+		if encoded_tree is None:
+			self.label = None
+			self.root = Node() # word count will be zero
+		else:
+			idx = encoded_tree.rfind('->')
+			label_part = encoded_tree[idx + 2:]
+			self.label = int(''.join(filter(type(label_part).isdigit, label_part)))
+			encoded_tree = encoded_tree[:idx]
+			self.root = self.tree_constructor(encoded_tree)
+	
+	def tree_constructor(self, encoded_tree, depth=0, parent=None):
+		encoded_tree = encoded_tree[1:-1]
+		node = Node()
+		node.depth = depth
+		node.parent = parent
+		
+		if '(' not in encoded_tree:
+			node.content = encoded_tree
+			node.wordcount = len(encoded_tree.split())
+			node.is_leaf = True
+			return node
+		
+		beg_pos, balance = 0, 0
+		segments = []
+		for pos in range(0, len(encoded_tree)):
+			if encoded_tree[pos] == '(': balance += 1
+			elif encoded_tree[pos] == ')': balance -= 1
+			if balance == 0:
+				segments.append((beg_pos, pos))
+				beg_pos = pos + 1
+		
+		node.children = []
+		for beg, end in segments:
+			child = self.tree_constructor(encoded_tree[beg: end + 1], depth=depth + 1, parent=node)
+			node.children.append(child)
+			node.wordcount += child.wordcount
+		
+		return node
+		
+class Node:
+	def __init__(self):
+		self.wordcount = 0
+		self.depth = None
+		self.content = None
+		self.parent = None
+		self.children = None
+		self.is_leaf = False
+			
+def read_trees(df, col_name='encoded_tree'):
+	if isinstance(df, pd.core.frame.DataFrame):
+		sys.stdout.write('Read trees:\n')
+		sys.stdout.flush()
+		trees = [Tree(tree_str) if col_name is not None else None for tree_str in tqdm(df[col_name])]
+		return trees
+	else:
+		sys.stdout.write('ERROR: input_ data is {type(input_)}, but the function admits pandas data frame type.')
+		sys.stdout.flush()
+		return None
+	
+def add_balanced_trees(df, col_name='encoded_tree', min_wordcount=50, deviation_ratio_from_mncls=1.5):
+	if not isinstance(df, pd.core.frame.DataFrame):
+		sys.stdout.write('ERROR: input_ data is {type(input_)}, but the function admits pandas data frame type.')
+		sys.stdout.flush()
+		
+	trees = read_trees(df, col_name)
+	trees = [tree if (tree is not None) and (tree.root.wordcount >= min_wordcount) else None for tree in trees]
+	count_cls = {}
+	for tree in trees:
+		if tree is None: continue
+		if tree.label not in count_cls: count_cls[tree.label] = 0
+		count_cls[tree.label] += 1
+	
+	mn = min(count_cls.values())
+	limit = deviation_ratio_from_mncls * mn
+	print(f'minimum = {mn}')
+	
+	count_cls = count_cls.fromkeys(count_cls, 0)
+	for i in range(len(trees)):
+		if trees[i] is None: continue
+		if count_cls[trees[i].label] >= limit: trees[i] = None
+		else: count_cls[trees[i].label] += 1
+		
+	df['trees'] = trees
+	df = df[df['trees'].notnull()]
+	return df
+
+def clean_text(text):
+	res = (text + '!')[:-1]
+	res = res.strip()
+	#for rep_ch in ['\\n', '\\r', '\\t', '\n', '\r', '\t', ')', '(']:
+	#	res = res.replace(rep_ch, ' ')
+	res = res.replace('\'', '')
+	res = re.sub(r'\\x..|\\xdd', '', res)
+	res = re.sub(r'\\s|\\n|\\r|\\t|\n|\r|\t|\)|\(', ' ', res)
+	res = res.replace('\x00','')
+	res = res.encode("ascii", errors="ignore").decode()
+	arr = res.split()
+	return ' '.join(arr)
+	
+def get_text_from_siblings_with_common_parent(node):
+	arr = []
+	for sb in node.next_siblings:
+		if isinstance(sb, NavigableString): continue
+		if sb.parent.name == node.parent.name:
+			txt = str(clean_text(sb.text))
+			arr.append(txt)
+	return ' '.join(arr)	
+		
+def traverse(node, cur_depth, cur_branch, cutoff_text, max_depth):
+	used_max_branches = cur_depth + 1
+	children = [child for child in node.children if isinstance(child, Tag)]
+	children = children[:used_max_branches]
+		
+	if len(children) == 0 or cur_depth == max_depth:
+		leaf_ret = '('
+		if node is not None:
+			txt = str(clean_text(node.text))
+			leaf_ret += txt
+			if cur_branch == used_max_branches - 1:
+				txt = str(get_text_from_siblings_with_common_parent(node))
+				leaf_ret = ' '.join([leaf_ret, txt])
+		
+		leaf_ret = ' '.join([leaf_ret, cutoff_text])		
+		leaf_ret = [s.strip() for s in leaf_ret.split()]
+		leaf_ret = ' '.join(leaf_ret)
+		leaf_ret += ')'
+		return None if len(leaf_ret) == 2 else leaf_ret
+
+	node_ret = ''
+	not_none_children = 0
+	for i, child in enumerate(children):            
+		passed_cutoff_text = ''
+		if i == len(children) - 1:
+			passed_cutoff_text = str(get_text_from_siblings_with_common_parent(child))
+			passed_cutoff_text = ' '.join([passed_cutoff_text, cutoff_text])
+			
+		child_ret = traverse(child, cur_depth + 1, i, passed_cutoff_text, max_depth)
+		if child_ret is not None:
+			node_ret += child_ret
+			not_none_children += 1
+	
+	if not_none_children == 0: return None
+	elif not_none_children == 1: return node_ret
+	else: return f'({node_ret})'
+			
+def html_to_encoded_tree(html_content, max_depth=4, label=None):
+	soup = BeautifulSoup(html_content, 'html.parser')
+	tree_line = traverse(soup, 0, 0, '', max_depth)
+	if tree_line is not None:
+		tree_line = tree_line + f'->{label}\n'
+	return tree_line
+
+def add_encoded_trees_to_dataframe(df):
+	df = df[(df['webpage_text'].notnull() & df['categories'].notnull() & (df['is_eng'] == True))]
+	classes = list(set(df['categories']))
+	df['class'] = df['categories'].apply(lambda x: classes.index(x))
+	df['encoded_tree'] = [html_to_encoded_tree(entry['webpage_text'], max_depth=4, label=entry['class']) for _, entry in tqdm(df.iterrows())]
+	df = add_balanced_trees(df)
+	return df
+
+def test():
+	df_raw = pd.read_csv('/home/vahidsanei_google_com/data/yelp_data/updated_large/business_with_corpus.csv')
+	df = unify_yelp_data_classes(df_raw)
+	classes = list(set(df['categories']))
+	df['categories'] = df['categories'].apply(lambda x: classes.index(x))
+	for i, entry in tqdm(df.iterrows()):
+		html = entry['webpage_text'] + '!'
+		html = html[:-1]
+		html_to_tree(html_content=html, label=entry['categories'], output_path_dir='/home/vahidsanei_google_com/data/yelp_data/trees/', is_new=True if i == 0 else False)		
+			
+if __name__ == '__main__':
+	tree_path = '/home/vahidsanei_google_com/data/yelp_data/trees/tree.txt'
+	#tree_path = '/home/vahidsanei_google_com/advertiser-quality-from-sites/source/recursive_model/trees/tree.txt'
+	trees = read_trees(tree_path)
+	balance_data(tree_path)
