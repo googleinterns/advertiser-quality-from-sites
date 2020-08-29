ea81bcc04d846732792439a68149992542b3da75
beginner1010 <vahid.sanei@gmail.com>
2020-08-29 01:30:39 +0000

    more unit tests

--- tests/test_tree_lib.py
+++ tests/test_tree_lib.py
@@ -4,7 +4,8 @@ import pytest
 
 @pytest.mark.parametrize('html_content, max_depth, label, exp_result', [ 
 	('<html> <title>A resturant</title> <h> <p>Menu</p> <h>Location</h> </html>', 4, 'food', '((A resturant)((Menu)(Location)))->food\n'),
-	('<html> <title>A resturant</title> <h> <p>Menu</p> <h>Location</h> </html>', 1, 'food', '(A resturant Menu Location)->food\n'),
+	('<html> <title>A resturant</title> <h> <p>Menu</p> <h>Location</h> </h> </html>', 1, 'food', '(A resturant Menu Location)->food\n'),
+	('<html> <title>Auto Zone</title> <h> <p>Menu</p> <h>Location</h> </h> </html>', 2, 'car', '((Auto Zone)(Menu Location))->car\n'),
 ])
 def test_html_to_encoded_tree(html_content, max_depth, label, exp_result):
     assert html_to_encoded_tree(html_content, max_depth, label) == exp_result
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,10 +8,12 @@ import pytest
 
 @pytest.mark.parametrize('txt, exp_result', [ 
 	('\r vahid \r \t\t\t\\t    sanei was an intern at \n Google\t\t', 'vahid sanei was an intern at Google'),
-	('\t\t\t Thanks YY and Xiang for helping \r                  me during this         internship! \t \n \r', 'Thanks YY and Xiang for helping me during this internship!')
+	('\t\t\t Thanks YY and Xiang for helping \r                  me during this         internship! \t \n \r', 'Thanks YY and Xiang for helping me during this internship!'),
+	('\t\t\t Advertiser\t\t    \r Quality from Their\n\t\rWebsites!!!', 'Advertiser Quality from Their Websites!!!')
 ])
 def test_clean_text(txt, exp_result):
     assert clean_text(txt) == exp_result
-    
+
+   
    
 
